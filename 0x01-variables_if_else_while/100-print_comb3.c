0e12a2476a6c439b4f87b0883d568f4237bbb08b
Moses Oduor <mosesoduor093@gmail.com>
2023-06-16 14:49:17 -0700

    combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			if (x != y && x < y)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

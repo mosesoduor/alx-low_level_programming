4aa20bf66dc45496d88685cbfe219f4793d8353b
Moses Oduor <mosesoduor093@gmail.com>
2023-06-16 14:04:59 -0700

    print hexadecimals, 0-9, a-f.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char letter;
+
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar('0' + x);
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}

dd38b98f98c92b7cab9928e88d30b33c27851984
Moses Oduor <mosesoduor093@gmail.com>
2023-06-16 17:23:46 -0700

    two two digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,21 @@
 
 int main(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 98; a++)
 	{
-	for (b = 0; b <= 9; b++)
+	for (b = a + 1; b <= 99; b++)
 	{
-	for (c = 0; c <= 9; c++)
-	{
-	for (d = 0; d <= 9; d++)
-	{
-		if (a <= c && a >= b && b <= c && d > 0 && b < d)
-		{
-			putchar(a + '0');
-			putchar(b + '0');
-			putchar(' ');
-			putchar(c + '0');
-			putchar(d + '0');
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	}
+		putchar((a / 10) + '0');
+		putchar((a % 10) + '0');
+		putchar(' ');
+		putchar((b / 10) + '0');
+		putchar((b % 10) + '0');
+		if (a == 98 && b == 99)
+		continue;
+		putchar(',');
+		putchar(' ');
 	}
 	}
 	putchar('\n');

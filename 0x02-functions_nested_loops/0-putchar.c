3d23129efba8371db5ac37ee78ed800de299ec19
Moses Oduor <mosesoduor093@gmail.com>
2023-06-20 12:10:49 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "_putchar.c"
-
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * Return: 0
  */
 
 int main(void)
 {
-	const char *message = "_putchar\n";
-	const char *i = message;
-
-	while (*i != '\0')
-	{
-		_putchar(*i);
-		i++;
-	}
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
--- 0x02-functions_nested_loops/0-putchar.h
+++
@@ -1,6 +0,0 @@
-#ifndef 0-PUTCHAR_H_
-#define 0-PUTCHAR_H_
-
-int _putchar(char c);
-
-#endif

510c9ba49e4f075d762c1eb4f2f3784095c0ac36
Moses Oduor <mosesoduor093@gmail.com>
2023-06-22 13:25:24 -0700

    product of a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - a function that multiplies two integers
+ * @a: int to multiply by b
+ * @b: int to multiply by a
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

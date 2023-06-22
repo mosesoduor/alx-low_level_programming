9db12fb49b1575611f6d9dd42e121f161f8fcb0a
Moses Oduor <mosesoduor093@gmail.com>
2023-06-22 15:05:21 -0700

    0 to 14 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - print 10x 0 - 14
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+			
+				_putchar('1');
+			_putchar((y % 10) + '0');
+			
+		}
+		_putchar('\n');
+		
+	}
+}

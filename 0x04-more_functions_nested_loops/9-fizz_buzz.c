3b7cf2a0dee1cde01950aa9e1aa94f4183784186
Moses Oduor <mosesoduor093@gmail.com>
2023-06-22 16:13:52 -0700

    fizz nuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,18 +6,20 @@
 
 int main(void)
 {
-	int x;
+	int i;
 
-	for (x = 1; x <= 100; x++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (((x % 3) == 0) && ((x % 5) == 0))
-			printf("FizzBuzz ");
-		else if ((x % 3) == 0)
-			printf("Fizz ");
-		else if ((x % 5) == 0)
-			printf("Buzz ");
-		else 
-			printf("%d ", x);
+		if (i % 3 == 0 && i % 5 != 0)
+			printf(" Fizz");
+		else if (i % 3 != 0 && i % 5 == 0)
+			printf(" Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
+		else if (i == 1)
+			printf("%d", i);
+		else
+			printf(" %d", i);
 	}
 	printf("\n");
 	return (0);

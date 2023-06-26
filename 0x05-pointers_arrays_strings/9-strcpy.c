558dd106568f1fdf48d9e2bb5b3cabea1bb341c9
Moses Oduor <mosesoduor093@gmail.com>
2023-06-26 15:26:14 -0700

    solve the problem, then write the code

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcpy -  function that copies the string pointed to by src
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: points to a buffer to be copied to
+ * @src: points to a string with contents to be copied
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(src + i) != '\0')
+		i++;
+	for (; j < i; j++)
+		dest[j] = src[j];
+	dest[i] = '\0';
+
+	return (dest);
+}
